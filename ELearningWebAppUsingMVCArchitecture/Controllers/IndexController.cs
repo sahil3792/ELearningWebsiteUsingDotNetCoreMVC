ac530234f0c4d1a28db0780af0d8de1b7235595f
sahil3792 <sahilharshalv@gmail.com>
2024-08-28 16:49:58 +0530

    Initial commit

---
+++ ELearningWebAppUsingMVCArchitecture/Controllers/IndexController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ELearningWebAppUsingMVCArchitecture.Controllers
+{
+	public class IndexController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			return View();
+		}
+		public IActionResult Courses()
+		{
+			return View();
+		}
+		public IActionResult Blog()
+		{
+			return View();
+		}
+	}
+}
