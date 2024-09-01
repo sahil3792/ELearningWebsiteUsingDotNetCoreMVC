1025028666d3df4abaaf6caf93b8f614de4ba709
sahil shaikh <sahilharshalv@gmail.com>
2024-09-02 02:31:26 +0530

    Video model complete

--- ELearningWebAppUsingMVCArchitecture/obj/Debug/net8.0/ELearningWebAppUsingMVCArchitecture.AssemblyInfo.cs
+++ ELearningWebAppUsingMVCArchitecture/obj/Debug/net8.0/ELearningWebAppUsingMVCArchitecture.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ELearningWebAppUsingMVCArchitecture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f3f28a346b99322b240b40da1b4686762849e4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d758f22f573c89b651cb36675cb13a5e548b63")]
 [assembly: System.Reflection.AssemblyProductAttribute("ELearningWebAppUsingMVCArchitecture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ELearningWebAppUsingMVCArchitecture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
